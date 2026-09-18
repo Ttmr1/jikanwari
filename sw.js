898ec2d20f4fda41c0aefbb9c0e86e1da0d7aba0
Ttmr1 <takayuki.20051012@gmail.com>
2026-09-19 04:00:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timetable-app-v7";
+const CACHE_NAME = "timetable-app-v10";
 const APP_SHELL = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
